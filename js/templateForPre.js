da9c3d23d6c005a16301cd1a937c0b1a3cba9be4
Liwen <wuliwen122@gmail.com>
2023-03-31 01:39:51 -0400

    template1

---
+++ js/templateForPre.js
@@ -0,0 +1,39 @@
+// class ButtonGroup extends HTMLElement {
+//     constructor() {
+//       super();
+//       const template = document.getElementById('button-template');
+//       const shadowRoot = this.attachShadow({mode: 'open'});
+//       shadowRoot.appendChild(template.content.cloneNode(true));
+//       shadowRoot.querySelector('.share-btn').addEventListener('click', () => {
+//         console.log('Share button clicked');
+//       });
+//       shadowRoot.querySelector('.favorite-btn').addEventListener('click', () => {
+//         console.log('Favorite button clicked');
+//       });
+//     }
+//   }
+  
+// customElements.define('button-group', ButtonGroup);
+
+const catTemplate = document.getElementById('item-template');
+const catContainer = document.getElementById('container');
+const apiKey = 'live_g2TlRXkZBAczbt1TkDyGIjgWk2Yd1wHzR7NahHoqdHGLa0bVho0uKmSl8u9otL1v';
+
+fetch(`https://api.thecatapi.com/v1/images/search?limit=10`, {
+  headers: {
+    'x-api-key': apiKey
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(cat => {
+      const catInstance = catTemplate.content.cloneNode(true);
+      catInstance.querySelector('img').src = cat.url;
+      catInstance.querySelector('h2').textContent = cat.id;
+      catInstance.querySelector('p').textContent = `Width: ${cat.width} Height: ${cat.height}`;
+      catContainer.appendChild(catInstance);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching cat images:', error);
+  });
